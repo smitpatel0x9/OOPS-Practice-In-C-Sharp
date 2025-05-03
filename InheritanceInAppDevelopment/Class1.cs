2d35aeab81f3d3610cb9f7f15716a0f1e26e939a
smitpatel0x9 <smitpapril@gmail.com>
2025-05-03 13:03:16 +0530

    Inheritance in Application Development

---
+++ InheritanceInAppDevelopment/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceInAppDevelopment
+{
+    public class Person
+    {
+        public int Id;
+        public string Name;
+        public string Address;
+        public string Phone;
+    }
+    public class Student : Person
+    {
+        public int Class;
+        public int Marks;
+        public string Grade;
+        public double Fees;
+    }
+    public class Staff : Person
+    {
+        public string Designation;
+        public double Salary;
+    }
+    public class TeachingStaff : Staff
+    {
+        public string Qualification;
+        public string Subject;
+    }
+    public class NonTeachingStaff : Staff
+    {
+        public string DeptName;
+        public int ManagerId;
+    }
+}
