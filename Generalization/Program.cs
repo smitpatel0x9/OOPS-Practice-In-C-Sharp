56f62f34889c0b085ec9509641d66bcc2dfeefd7
smitpatel0x9 <smitpapril@gmail.com>
2025-05-04 18:15:23 +0530

    Generalization concept

---
+++ Generalization/Program.cs
@@ -0,0 +1,40 @@
+﻿public interface ICar
+{
+    void Start();
+    void Stop();
+}
+public class Innova : ICar
+{
+    public void Start()
+    {
+        Console.WriteLine("Innova Start");
+    }
+    public void Stop()
+    {
+        Console.WriteLine("Innova Stop");
+    }
+}
+public class BMW : ICar
+{
+    public void Start()
+    {
+        Console.WriteLine("BMW Start");
+    }
+    public void Stop()
+    {
+        Console.WriteLine("BMW Stop");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Innova c1 = new();
+        c1.Start();
+        c1.Stop();
+
+        BMW c2 = new();
+        c2.Start();
+        c2.Stop();
+    }
+}
