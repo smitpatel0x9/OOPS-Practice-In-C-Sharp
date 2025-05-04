f4ee43530fc905f8ffedb7ede058d3edfb024e38
smitpatel0x9 <smitpapril@gmail.com>
2025-05-04 13:01:48 +0530

    IsA Relationship Demo

---
+++ IsA/Program.cs
@@ -0,0 +1,11 @@
+﻿using IsADemo;
+public class Program
+{
+    public static void Main()
+    {
+        Cuboid c1 = new(3, 4, 5);
+        Console.WriteLine($"Volume : {c1.Volume()}");
+        Console.WriteLine($"Area : {c1.Area()}");
+        Console.WriteLine($"Perimeter : {c1.Perimeter()}");
+    }
+}
---
+++ IsADemo/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsADemo
+{
+    public class Rectangle
+    {
+        public double _length;
+        public double _width;
+
+        public double Area() => _length * _width;
+
+        public double Perimeter() => 2 * (_length + _width);
+    }
+
+    public class Cuboid : Rectangle // Establishing Parent-Child Relationship
+                                    // IsA Relationship : Cuboid IsA a Rectangle
+    {
+        double _height;
+        public Cuboid(double length, double width, double height)
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+        }
+
+        public double Volume() => _length * _width * _height;
+    }
+}
