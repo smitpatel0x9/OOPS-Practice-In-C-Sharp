38a37d445ea85c6b0d6926a1bf66d68597d9fbf6
smitpatel0x9 <smitpapril@gmail.com>
2025-05-05 12:49:26 +0530

    Added some important comments

--- AbstractClassDemo/Program.cs
+++ AbstractClassDemo/Program.cs
@@ -22,6 +22,10 @@ public class AbsChild : AbsParent
     {
         Console.WriteLine($"Division of {x} and {y} is {x / y}");
     }
+    public void Something()
+    {
+        Console.WriteLine("Something");
+    }
 }
 
 public class Program
@@ -30,9 +34,10 @@ public class Program
     {
         AbsChild obj = new(); // Child class instance     
         AbsParent obj2 = obj; // Abstract class reference poiting to child class object
-        obj2.Add(10, 5);
+        obj2.Add(10, 5); // Abstract class reference can access abstract class non-abstract method
         obj2.Sub(10, 5);
-        obj2.Mul(10, 5);
+        obj2.Mul(10, 5); // Abstract class reference can access abstract class abstract method
         obj2.Div(10, 5);
+        //obj2.Something(); Abstract class reference cannot access pure child class methods
     }
 }
