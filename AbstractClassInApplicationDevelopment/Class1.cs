8bcf344265e421ceaa1a7b8e4f420af3048e6e60
smitpatel0x9 <smitpapril@gmail.com>
2025-05-06 12:38:42 +0530

    Abstract Class use in C# Application Development

---
+++ AbstractClassInApplicationDevelopment/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassInApplicationDevelopment
+{
+    public abstract class Shape
+    {
+        public double _height;
+        public double _width;
+        public double _radius;
+        public const double _PI = 3.14f;
+        public abstract double Area();
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle(int height,int width)
+        {
+            _height = height;
+            _width = width;
+        }
+        public override double Area() => _height * _width;
+    }
+    public class Circle : Shape
+    {
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+        public override double Area() => _PI * Math.Pow(_radius, 2);
+    }
+    public class Triangle : Shape
+    {
+        public Triangle(int width,int height)
+        {
+            _width = width;
+            _height = height;
+        }
+        public override double Area() => 0.5 * _width * _height;
+    }
+    public class Cone : Shape
+    {
+        public Cone(int height,int radius)
+        {
+            _height = height;
+            _radius = radius;
+        }
+        public override double Area() => (1 / 3.0) * _PI * Math.Pow(_radius, 2) * _height;
+    }
+}
---
+++ AbstractClassUseInAppDev/Program.cs
@@ -0,0 +1,21 @@
+﻿using AbstractClassInApplicationDevelopment;
+namespace AbstractClassUseInAppDev
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle obj1 = new(3, 4);
+            Console.WriteLine($"Area of Rectangle : {obj1.Area()}");
+
+            Circle obj2 = new(4);
+            Console.WriteLine($"Area of Circle : {obj2.Area()}");
+
+            Triangle obj3 = new(5, 6);
+            Console.WriteLine($"Area of Triangle : {obj3.Area()}");
+
+            Cone obj4 = new(3, 4);
+            Console.WriteLine($"Area of Cone : {obj4.Area()}");
+        }
+    }
+}
