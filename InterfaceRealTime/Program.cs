db31f0e27ea907afd76370d84565d2ee49b502fe
smitpatel0x9 <smitpapril@gmail.com>
2025-05-08 16:56:01 +0530

    Completed Interface Realtime Demo

--- InterfaceRealTime/Program.cs
+++ InterfaceRealTime/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IBankAccount account1 = new SavingsAccount();
+            account1.DepositAmount(1000);
+            account1.WithdrawAmount(500);
+            account1.DepositAmount(-50);
+            account1.WithdrawAmount(1000);
+            account1.DepositAmount(10000);
+            account1.WithdrawAmount(9700);
+            account1.WithdrawAmount(3000);
         }
     }
     interface IBankAccount
@@ -16,7 +23,7 @@
     public class SavingsAccount : IBankAccount
     {
         private decimal _balance;
-        private decimal _perDayWithdrawLimit;
+        private decimal _perDayWithdrawLimit = 10000;
         private decimal _todayWithdraw;
         public bool DepositAmount(decimal amount)
         {
@@ -26,7 +33,7 @@
                 return false;
             }
             _balance += amount;
-            Console.WriteLine($"You have deposited {amount}");
+            Console.WriteLine($"You have deposited {amount} in your savings account");
             Console.WriteLine($"Balance : {_balance}");
             return true;
         }
@@ -34,7 +41,7 @@
         {
             if (amount > _balance)
             {
-                Console.WriteLine("Insufficient balance");
+                Console.WriteLine("Insufficient balance in your savings account");
                 return false;
             }
             if(_todayWithdraw + amount > _perDayWithdrawLimit)
@@ -43,10 +50,40 @@
                 return false;
             }
             _balance -= amount;
-            Console.WriteLine($"You have withdrawn {amount}");
+            _todayWithdraw += amount;
+            Console.WriteLine($"You have withdrawn {amount} from your savings account");
             Console.WriteLine($"Balance : {_balance}");
             return true;
         }
-        public decimal 
+        public decimal CheckBalance() => _balance;
+    }
+    public class CurrentAccount : IBankAccount
+    {
+        private decimal _balance;
+        public bool DepositAmount(decimal amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Amount should be greater than zero");
+                return false;
+            }
+            _balance += amount;
+            Console.WriteLine($"You have deposited {amount} in your current account");
+            Console.WriteLine($"Balance : {_balance}");
+            return true;
+        }
+        public bool WithdrawAmount(decimal amount)
+        {
+            if (amount > _balance)
+            {
+                Console.WriteLine("Insufficient balance in your current account");
+                return false;
+            }
+            _balance -= amount;
+            Console.WriteLine($"You have withdrawn {amount} from your savings account");
+            Console.WriteLine($"Balance : {_balance}");
+            return true;
+        }
+        public decimal CheckBalance() => _balance;
     }
 }
