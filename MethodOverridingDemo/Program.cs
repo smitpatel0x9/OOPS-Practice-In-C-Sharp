cc75bb6a8c2626a0cf8518fb6e038fe37eeacea1
smitpatel0x9 <smitpapril@gmail.com>
2025-05-12 14:45:39 +0530

    Method Overriding Demo

---
+++ MethodOverridingDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodOverridingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new();
+            obj1.Show();
+
+            Class2 obj2 = new();
+            obj2.Show();
+        }
+    }
+    public class Class1
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Method of class 1");
+        }
+    }
+    public class Class2 : Class1
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Method of class 2");
+        }
+    }
+}
