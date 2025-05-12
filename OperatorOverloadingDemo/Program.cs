8dee73e223f9de0457732030705ff884aba5e84e
smitpatel0x9 <smitpapril@gmail.com>
2025-05-12 11:41:36 +0530

    Operator Overloading Demo

---
+++ OperatorOverloadingDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OperatorOverloadingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new(3, 5);
+            c1.Display();
+            Complex c2 = new(2, 3);
+            c2.Display();
+            Complex obj = Complex.Add(c1, c2);
+            obj.Display();
+        }
+    }
+    public class Complex
+    {
+        private int _real;
+        private int _imaginary;
+        public Complex(int real = 0, int imaginary = 0)
+        {
+            _real = real;
+            _imaginary = imaginary;
+        }
+        public static Complex Add(Complex c1,Complex c2)
+        {
+            Complex temp = new();
+            temp._real = c1._real + c2._real;
+            temp._imaginary = c1._imaginary + c2._imaginary;
+            return temp;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{_real} + {_imaginary}i");
+        }
+    }
+}
