579b8f75bc7e7b7c6028f33e079396fb5cb85201
smitpatel0x9 <smitpapril@gmail.com>
2025-05-12 17:31:59 +0530

    Method Hiding Demo

---
+++ MethodHidingDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MethodHidingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child obj1 = new Child();
+            obj1.Show();
+            obj1.Display();
+        }
+    }
+    public class Parent
+    {
+        public virtual void Show() // Virtual method
+        {
+            Console.WriteLine("Parent class Show method");
+        }
+        public void Display() // non-virtual method
+        {
+            Console.WriteLine("Parent class Display method");
+        }
+    }
+    public class Child : Parent
+    {
+        public override void Show() // Method Overriding - Show() overridden by child class
+        {
+            Console.WriteLine("Child class Show method");
+        }
+        public new void Display() // Method Hiding - Display() hidden by child class
+        {
+            Console.WriteLine("Child class Display method");
+        }
+    }
+}
