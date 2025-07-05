2eca9014fcedc8d75dbf180d89468b57498e616f
= <=>
2025-07-05 10:21:03 +0530

    updated CopyConstructor

--- Practice/CopyConstructor.cs
+++ Practice/CopyConstructor.cs
@@ -8,6 +8,8 @@ public class CopyConstructor
         check1.Display();
         Test check2 = new(check1);
         check2.Display();
+        Test check3 = new(check1,check2);
+        check3.Display();
     }
 }
 
@@ -21,6 +23,10 @@ public class Test
     public Test(Test obj) // Copy Constructor
     {
         x = obj.x;  // You are using previous object's x value and assigning it to current object x value
+    }
+    public Test(Test obj1, Test obj2)
+    {
+        x = obj1.x + obj2.x;
     }
     public void Display()
     {
